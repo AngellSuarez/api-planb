e7a3a725d0e966ee5d45266d3bad7729de7055ac
AngellSuarez <miguel.angel.cardona.suarez1@gmail.com>
2025-01-02 21:45:33 -0500

    new routes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const cors = require("cors")
 //routes
 const userRouter = require("./api/users/user.router")
 const adminRouter = require("./api/administrador/admin.router")
+const negocioRouter = require("./api/negocios/negocio.router")
+const paseadoRouter = require("./api/paseadores/paseador.router")
+const searchRouter = require("./api/buscadorGeneral/buscador.router")
 
 app.get("/api", (req,res)=>{
     res.json({
@@ -19,6 +22,9 @@ app.use(express.json());
 app.use(cors())
 app.use("/api/users/",userRouter);
 app.use("/api/admin/",adminRouter);
+app.use("/api/negocios/",negocioRouter);
+app.use("/api/paseador/",paseadoRouter);
+app.use("/api/search/",searchRouter);
 
 app.listen(process.env.APP_PORT,()=>{
     console.log("Server up and running on PORT:",process.env.APP_PORT)
